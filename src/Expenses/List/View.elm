4987288d48208086477f098c06edaec9c6628be1
Gustavo Saiani <gustavo@poe.ma>
2017-01-25 17:41:09 -0200

    Expenses List View

--- src/Expenses/List/View.elm
+++ src/Expenses/List/View.elm
@@ -47,3 +47,93 @@ expenseItemLinkText expense =
                     ""
     in
         title
+
+
+expenseItem : Expense -> Html Msg
+expenseItem expense =
+    tr [ style [ ( "cursor", "pointer" ) ], onClick (Show expense) ]
+        [ td []
+            [ span [ class "date" ]
+                [ div [ class "date-header" ] [ text (Date.month expense.createdAt |> toString) ]
+                , div [ class "date-day" ] [ text (Date.day expense.createdAt |> toString) ]
+                ]
+            ]
+        , td [] [ text (expenseItemLinkText expense) ]
+        , td [] [ text expense.createdByName ]
+        , td [ class "text-right" ] [ text (formatAmount expense.amount) ]
+        ]
+
+
+viewExpenseList : List Expense -> String -> Html Msg
+viewExpenseList filteredExpenses totalString =
+    div [ class "clear col-12 mt1" ]
+        [ table []
+            [ tbody [] (List.map expenseItem filteredExpenses)
+            , tfoot []
+                [ tr []
+                    [ th [] [ text "" ]
+                    , th [] [ text "" ]
+                    , th [] [ text "Total:" ]
+                    , th [ class "text-right" ] [ text totalString ]
+                    ]
+                ]
+            ]
+        ]
+
+
+viewExpenseForm : Model -> Html Msg
+viewExpenseForm model =
+    div [ class "clear" ]
+        [ div [ class "col-12" ]
+            [ div [ id "amount" ]
+                [ span [] [ text (formatAmount ((model.amount |> toFloatPoh) / 100.0)) ]
+                , input
+                    [ id "amount-input"
+                    , type_ "number"
+                    , step "1"
+                    , name "amount"
+                    , value model.amount
+                    , pattern "[0-9]*"
+                    , placeholder "Apple's Safari sucks!"
+                    , autocomplete False
+                    , onInput AmountInput
+                    ]
+                    []
+                ]
+            ]
+        ]
+
+
+viewBudgets : User -> List Expense -> Model -> Html Msg
+viewBudgets user expenses model =
+    Html.map BudgetList (Budgets.root model.weekNumber expenses model.buttons)
+
+
+weekHeader : Model -> String -> Html Msg
+weekHeader model total =
+    let
+        weekName =
+            if model.weekNumber == 0 then
+                "This week"
+            else if model.weekNumber == -1 then
+                "Last week"
+            else
+                (toString -model.weekNumber) ++ " weeks ago"
+
+        rightDisabledClass =
+            if model.weekNumber == 0 then
+                " disabled"
+            else
+                ""
+    in
+        div [ class "clear mt2" ]
+            [ div [ class "col-3 col-1-hd" ]
+                [ button [ class "button week-button", onClick LoadPreviousWeek ] [ text "<<" ]
+                ]
+            , div [ class "col-6 col-10-hd" ]
+                [ div [ class "week-header" ][ text (weekName ++ " - " ++ total) ]
+                ]
+            , div [ class "col-3 col-1-hd" ]
+                [ button [ class ("button week-button" ++ rightDisabledClass), onClick LoadNextWeek ] [ text ">>" ]
+                ]
+            ]
